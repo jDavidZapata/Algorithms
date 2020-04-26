60bc64773325de041327ad41d5c5bd4e61a255fe
Juan David Zapata <jdavidzapata1983@gmail.com>
2020-04-25 23:53:56 -0400

    Linear Search

---
+++ AlgorithmsInC/LinearSearch.c
@@ -0,0 +1,30 @@
+// Linear Search
+
+/*
+LinearSearch(array, key)
+  for each item in the array
+    if item == value
+      return its index
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int search(int array[], int n, int key)
+{
+  for (int i = 0; i < n; i++)
+    if (array[i] == key)
+      return i;
+  return -1;
+}
+
+int main()
+{
+  int array[] = {2, 4, 60, 8, 3, 5, 1};
+  int x = 3;
+  int n = sizeof(array) / sizeof(array[0]);
+
+  int result = search(array, n, x);
+
+  (result == -1) ? printf("Can not find Element.") : printf("Element found at index: %d\nElement is = %d", result, array[result]);
+}
