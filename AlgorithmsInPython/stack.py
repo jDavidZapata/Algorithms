9c3ff2f49ef9a10c332fd571591c4155341d0233
Juan David Zapata <jdavidzapata1983@gmail.com>
2020-04-28 23:59:37 -0400

    stack

---
+++ AlgorithmsInPython/stack.py
@@ -0,0 +1,21 @@
+
+# Stack implementation in Python
+
+
+stack = []
+
+# Add elements into stack
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print('Stack')
+print(stack)
+
+# Popping elements from stack
+print('\nAfter popping an element from the stack:')
+print(stack.pop())
+print(stack)
+
+print('\nStack after elements are poped:')
+print(stack)
